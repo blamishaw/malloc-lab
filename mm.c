b25d143b589cc59be3fde94d7ccf3e062e3d3c7f
Brendan Lamishaw <brendanlamishaw@Brendans-MacBook-Pro.local>
2020-03-06 16:06:29 -0600

    old commit

--- mm.c
+++ mm.c
@@ -257,24 +257,17 @@ static void *coalesce(void *bp){
 }
 
 /* Helper function to find a free block in the heap
-   This uses the best-fit method
+   Uses the first-fit method
 */
 
 static void *find_fit(size_t asize){
     void *bp;
-    void *smallest_so_far = heap_listp;
     
     for (bp = heap_listp; GET_SIZE(HDRP(bp)) > 0; bp = NEXT_BLKP(bp)){
         if (!GET_ALLOC(HDRP(bp)) && (asize <= GET_SIZE(HDRP(bp)))) {
-            if (GET_SIZE(HDRP(bp)) < GET_SIZE(HDRP(smallest_so_far))) {
-                smallest_so_far = bp;
-            }
+            return bp;
         }
     }
-    if (smallest_so_far != heap_listp){
-        return smallest_so_far;
-    }
-    
     return NULL;
 }
 
