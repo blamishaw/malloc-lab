d678d8f05290d308cc86db128c4843844dc16afa
Brendan Lamishaw <brendanlamishaw@Brendans-MacBook-Pro.local>
2020-03-17 17:42:28 -0700

    try

--- mm.c
+++ mm.c
@@ -425,10 +425,10 @@ static void removeBlock(void *bp){
     /* If bp is somewhere in the middle of the list */
     else if (GET_PREV_FREE(bp) != NULL && GET_NEXT_FREE(bp) != NULL){
         /* Set next of prev block to next block */
-        PACK_NEXT(GET_PREV_FREE(bp), GET_NEXT_FREE(bp));
+        GET_PREV_FREE(GET_NEXT_FREE(bp)) = GET_PREV_FREE(bp);
         
         /* Set prev of next block to prev block*/
-        PACK_PREV(GET_NEXT_FREE(bp), GET_PREV_FREE(bp));
+        GET_NEXT_FREE(GET_PREV_FREE(bp)) = GET_NEXT_FREE(bp);
 
     }
     
